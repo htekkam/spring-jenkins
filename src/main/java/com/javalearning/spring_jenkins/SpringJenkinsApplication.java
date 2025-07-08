96882a52ccf356d217b471057f5f17f046741670
harikrishna t <harikrishna.tekkam@gmail.com>
2025-07-08 10:51:42 +0530

    added code to check jenkins build

--- src/main/java/com/javalearning/spring_jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/javalearning/spring_jenkins/SpringJenkinsApplication.java
@@ -19,6 +19,7 @@ public class SpringJenkinsApplication {
 
 	public static void main(String[] args) {
 		logger.info("Application executed");
+		logger.info("checking jenkins continous build");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
