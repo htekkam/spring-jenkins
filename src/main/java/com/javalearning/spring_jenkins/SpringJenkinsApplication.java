e75755732722b887783072035e21af5d2f6b1d9c
harikrishna t <harikrishna.tekkam@gmail.com>
2025-07-08 13:06:28 +0530

    added test code to check email notification

--- src/main/java/com/javalearning/spring_jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/javalearning/spring_jenkins/SpringJenkinsApplication.java
@@ -20,7 +20,7 @@ public class SpringJenkinsApplication {
 	public static void main(String[] args) {
 		logger.info("Application executed");
 		logger.info("checking jenkins continous build  ");
-		logger.info("checking email notification from jenkins ");
+		//logger.info("checking email notification from jenkins ");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
